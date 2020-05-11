20cb43b0ad55b8f0c06c583098b0ccbf476c11b6
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-05-11 20:06:24 +0530

    search done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lectures: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:1234/lecture/get_lec";
+    axios.get(url).then((res) => {
+      this.setState({
+        lectures: res.data,
+      });
+    });
+  }
+
+  render() {
+    const child = this.state.lectures.map((el, index) => {
+      if (el.lectureName.match(/Flutter/gi)) {
+        return (
+          <div key={index}>
+            <p>Lecture Name - {el.lectureName}</p>
+            <p>Number of Pages- {el.slideNumber}</p>
+            <p>Content- {el.content[4]}</p>
+            <hr />
+            {/* <p>slideNumber - {el.lectures.slideNumber}</p> */}
+          </div>
+        );
+      } else {
+        return null;
+      }
+    });
+    return (
+      <div>
+        <div>{child}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
