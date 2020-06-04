c8cab2ef30f299f64b5fa37c9455bd31bfb93898
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-06-04 06:21:18 +0530

    added mtit

--- src/App.js
+++ src/App.js
@@ -138,7 +138,9 @@ class App extends React.Component {
                 <option value="lecture">CTSE (10/10 Lectures Processed)</option>
                 <option value="mllecture">ML (01/10 Lectures Processed)</option>
                 <option value="dslecture">DS (08/08 Lectures Processed)</option>
-                <option value="lecture">MTIT (00/10 Lectures Processed)</option>
+                <option value="mtitlecture">
+                  MTIT (08/08 Lectures Processed)
+                </option>
                 <option value="lecture">QA (00/10 Lectures Processed)</option>
               </Form.Control>
             </Nav>
