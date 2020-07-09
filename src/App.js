4a7252b9fa76e010d345a770b54e4dc832a8e6bf
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-07-09 23:06:34 +0530

    finance added

--- src/App.js
+++ src/App.js
@@ -152,6 +152,7 @@ class App extends React.Component {
                   Econ (09/09 Lectures Processed)
                 </option>
                 <option value="oblecture">OB (09/09 Lectures Processed)</option>
+                <option value="fnlecture">OB (06/06 Lectures Processed)</option>
               </Form.Control>
             </Nav>
             <Nav>
